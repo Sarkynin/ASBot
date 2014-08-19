49d97d3e99bf83c7d87853a4fad1cec68cd5b868
apt-get <badr.yacoubi@gmail.com>
2014-08-19 01:55:49 +0100

    I'm so sorry I swear this is last time

--- plugins/roll-plugin.rb
+++ plugins/roll-plugin.rb
@@ -11,22 +11,23 @@ class RollPlugin
     rng = Random.new
     if numberofdices.to_i >= 150 && msgtype == '@'
       BotUtils.msgtype_reply(m, msgtype, "The roll is too big. Please consider using less than 150 rolls multiple times to achieve your result.")
-    end
-    rollnumber.length == 0 ? rollnumber = 10000 : rollnumber = rollnumber.to_i
-    numberofdices.length == 0 ? numberofdices = 1 : numberofdices = numberofdices.to_i
-    if numberofdices == 1
-      rollednumber = rng.rand(1..rollnumber)
-      BotUtils.msgtype_reply(m, msgtype, "Rolled between 1 and #{rollnumber}: #{rollednumber}")
     else
-      numberarray = Array.new
-      numbersstring = String.new
-      numberofdices.times do
-        numberarray << rng.rand(1..rollnumber)
+      rollnumber.length == 0 ? rollnumber = 10000 : rollnumber = rollnumber.to_i
+      numberofdices.length == 0 ? numberofdices = 1 : numberofdices = numberofdices.to_i
+      if numberofdices == 1
+        rollednumber = rng.rand(1..rollnumber)
+        BotUtils.msgtype_reply(m, msgtype, "Rolled between 1 and #{rollnumber}: #{rollednumber}")
+      else
+        numberarray = Array.new
+        numbersstring = String.new
+        numberofdices.times do
+          numberarray << rng.rand(1..rollnumber)
+        end
+        numberarray.each do |rollednumber|
+          numbersstring << "#{rollednumber} "
+        end
+        BotUtils.msgtype_reply(m, msgtype, "Rolled #{numberofdices} dice between 1 and #{rollnumber}: #{numbersstring}")
       end
-      numberarray.each do |rollednumber|
-        numbersstring << "#{rollednumber} "
-      end
-      BotUtils.msgtype_reply(m, msgtype, "Rolled #{numberofdices} dice between 1 and #{rollnumber}: #{numbersstring}")
     end
   end
 
