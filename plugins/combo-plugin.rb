aecadb87d45420f76dcf16eb07b62d3f74ce0d30
apt-get <badr.yacoubi@gmail.com>
2014-08-20 22:35:58 +0100

    fix rounding

--- plugins/combo-plugin.rb
+++ plugins/combo-plugin.rb
@@ -9,12 +9,12 @@ class ComboPlugin
 
   def combosame(m, msgtype, energy)
     energycost = (energy.to_f + 2) * 3.5
-    BotUtils.msgtype_reply(m, msgtype, "Energy cost: #{energycost.round}")
+    BotUtils.msgtype_reply(m, msgtype, "Energy cost: #{energycost.round(1)}")
   end
 
   def combodifferent(m, msgtype, energy1, energy2)
     energycost = (energy1.to_f + energy2.to_f) * 1.5
-    BotUtils.msgtype_reply(m, msgtype, "Energy cost: #{energycost.round}")
+    BotUtils.msgtype_reply(m, msgtype, "Energy cost: #{energycost.round(1)}")
   end
 
 end
