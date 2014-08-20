5aea46f9cfd25d9a209d80768b48446adabfc12f
apt-get <badr.yacoubi@gmail.com>
2014-08-20 22:42:29 +0100

    fix

--- plugins/combo-plugin.rb
+++ plugins/combo-plugin.rb
@@ -9,11 +9,21 @@ class ComboPlugin
 
   def combosame(m, msgtype, energy)
     energycost = (energy.to_f + 2) * 3.5
+    if energycost.round(1) - energycost.to_i == 0.5
+      energycost = energycost.round + 0.5
+    else
+      energycost = energycost.round
+    end
     BotUtils.msgtype_reply(m, msgtype, "Energy cost: #{energycost.round(1)}")
   end
 
   def combodifferent(m, msgtype, energy1, energy2)
     energycost = (energy1.to_f + energy2.to_f) * 1.5
+    if energycost.round(1) - energycost.to_i == 0.5
+      energycost = energycost.round + 0.5
+    else
+      energycost = energycost.round
+    end
     BotUtils.msgtype_reply(m, msgtype, "Energy cost: #{energycost.round(1)}")
   end
 
