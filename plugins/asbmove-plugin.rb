2756ca588cb291c772234fb55afc35731b79010d
apt-get <badr.yacoubi@gmail.com>
2014-08-22 20:05:39 +0100

    ok

--- plugins/asbmove-plugin.rb
+++ plugins/asbmove-plugin.rb
@@ -13,34 +13,32 @@ class ASBMovePlugin
     twolinemoves = ["stealthrock", "copycat", "crushgrip", "detect", "doubleteam", "eruption", "fairylock", "finalgambit", "flash", "guardsplit", "gyroball", "healorder", "helpinghand", "kinesis", "kingsshield", "metronome", "milkdrink", "mirrormove", "moonlight", "morningsun", "mudslap", "naturepower", "painsplit", "powersplit", "protect", "punishment", "recover", "roost", "sandattack", "seismictoss", "slackoff", "sleeptalk", "snatch", "softboiled", "spikyshield", "stealthrock", "steamroller", "stomp", "storedpower", "submission", "synthesis", "vitalthrow", "waterspout", "wish"]
     tablemoves = ["electroball", "flail", "fling", "grassknot", "lowkick", "magnitude", "present", "reversal", "substitute"]
 
-    rowcounter = -1
     movefound = false
-    $movesheet.rows.each do |row|
-      rowcounter += 1
+    $movesheet.rows.each.with_index do |row, index|
       if BotUtils.condense_name(row[0].gsub(" (Move)", "")) == BotUtils.condense_name(move)
-        BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter][0].sub(" (Move)", "")} - #{$movesheet.rows[rowcounter][1]} | #{$movesheet.rows[rowcounter][2]} | #{$movesheet.rows[rowcounter][3]} | #{$movesheet.rows[rowcounter][6]} BAP | #{$movesheet.rows[rowcounter][8]} Acc | #{$movesheet.rows[rowcounter][9]} EN Cost | #{$movesheet.rows[rowcounter][10]} Eff% | Contact: #{$movesheet.rows[rowcounter][11]} | #{$movesheet.rows[rowcounter][12]} Prio | Combo Type: #{$movesheet.rows[rowcounter][13]} | Snatch: #{$movesheet.rows[rowcounter][14]} | Magic Coat: #{$movesheet.rows[rowcounter][15]}")
+        BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index][0].sub(" (Move)", "")} - #{$movesheet.rows[index][1]} | #{$movesheet.rows[index][2]} | #{$movesheet.rows[index][3]} | #{$movesheet.rows[index][6]} BAP | #{$movesheet.rows[index][8]} Acc | #{$movesheet.rows[index][9]} EN Cost | #{$movesheet.rows[index][10]} Eff% | Contact: #{$movesheet.rows[index][11]} | #{$movesheet.rows[index][12]} Prio | Combo Type: #{$movesheet.rows[index][13]} | Snatch: #{$movesheet.rows[index][14]} | Magic Coat: #{$movesheet.rows[index][15]}")
         
         if fourlinemoves.include?(BotUtils.condense_name(move))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+1][1]}".gsub("é","e"))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+2][1]}".gsub("é","e"))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+3][1]}".gsub("é","e"))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+4][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+1][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+2][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+3][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+4][1]}".gsub("é","e"))
 
         elsif threelinemoves.include?(BotUtils.condense_name(move))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+1][1]}".gsub("é","e"))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+2][1]}".gsub("é","e"))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+3][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+1][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+2][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+3][1]}".gsub("é","e"))
 
         elsif twolinemoves.include?(BotUtils.condense_name(move))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+1][1]}".gsub("é","e"))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+2][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+1][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+2][1]}".gsub("é","e"))
 
         elsif tablemoves.include?(BotUtils.condense_name(move))
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+1][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+1][1]}".gsub("é","e"))
           BotUtils.msgtype_reply(m, msgtype, "Please check the NDA for the table.")
 
         else
-          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[rowcounter+1][1]}".gsub("é","e"))
+          BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+1][1]}".gsub("é","e"))
         end
         movefound = true
       end
