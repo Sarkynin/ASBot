8e73e138f586b224d972b179f83577c0164c726b
apt-get <badr.yacoubi@gmail.com>
2014-08-25 19:28:22 +0100

    fix no, and add yes

--- plugins/no-plugin.rb
+++ plugins/no-plugin.rb
@@ -3,12 +3,21 @@ require 'cinch'
 class NoPlugin
   include Cinch::Plugin
   
-  match "!no"
+  match /^!no$/i,      method: :disapproval
+  match "... pff",     method: :pff
 
-  def execute(m)
+  def disapproval(m)
     m.reply("Never, not in a million years, absolutely not, no way Jose, no chance Lance, nyet, negatory, mm-mm, nuh-uh, uh-uh! And of course my own personal favourite of all time, man falling off of a cliff: NNNOOOOOOOOOoooooooo.......")
-    sleep 15
-    m.reply("... pff")
+    @pfftimer = Timer(15, :shots => 1) {
+      m.reply("... pff")
+    }
+  end
+
+  def pff(m)
+    unless @pfftimer.stopped? || @pfftimer == nil
+      @pfftimer.stop
+      m.reply("Hey, that's my line!")
+    end
   end
 
 end
---
+++ plugins/yes-plugin.rb
@@ -0,0 +1,23 @@
+require 'cinch'
+
+class YesPlugin
+  include Cinch::Plugin
+  
+  match /^!yes$/i,     method: :approval
+  match "... sss",     method: :sss
+
+  def approval(m)
+    m.reply("Always, yes in a million years, absolutely so, yes way Jose, yes chance Lance, dah, affirmitive, mm-hmm, yuh-huh, uh-huh! And of course my own personal favourite of all time, man winning the lottery: YYYEEEEEEEEEssssssss.......")
+    @ssstimer = Timer(15, :shots => 1) {
+      m.reply("... sss")
+    }
+  end
+
+  def sss(m)
+    unless @ssstimer.stopped? || @ssstimer == nil
+      @ssstimer.stop
+      m.reply("Hey, that's my line!")
+    end
+  end
+
+end
