ff569365d9f3cd61f11f78cad85cf7458013111e
apt-get <badr.yacoubi@gmail.com>
2014-09-03 17:16:46 +0100

    fix

--- plugins/learn-plugin.rb
+++ plugins/learn-plugin.rb
@@ -13,22 +13,22 @@ class LearnPlugin
 
   def execute(m, msgtype, pokemon, move)
 
-    pokerow = @db[:pokemon].filter(:identifier => BotUtils.condense_name(pokemon))
+    pokerow = @db[:pokemon].filter(:identifier => BotUtils.condense_name(pokemon)).first
     if pokerow.nil?
       BotUtils.msgtype_reply(m, msgtype, "Pokémon not found.")
       return
     end
     pokeid = pokerow[:id]
-    pokename = @db[:pokemon_species_names].filter(:local_language_id => 9, :pokemon_species_id => pokerow[:species_id])
+    pokename = @db[:pokemon_species_names].filter(:local_language_id => 9, :pokemon_species_id => pokerow[:species_id]).first
 
     moveid = @db[:moves].filter(:identifier => BotUtils.condense_name(move)).first
     if moveid.nil?
       BotUtils.msgtype_reply(m, msgtype, "Move not found.")
       return
     end
-    movename  = @db[:move_names].filter(:local_language_id => 9, :move_id => moveid)
+    movename  = @db[:move_names].filter(:local_language_id => 9, :move_id => moveid).first
 
-    movefound = !@db[:pokemon_moves].filter(:pokemon_id => pokeid, :move_id => moveid).nil?
+    movefound = !@db[:pokemon_moves].filter(:pokemon_id => pokeid, :move_id => moveid).first.nil?
 
     if movefound
       string = "#{pokename} #{Format(:green, "can")} learn #{movename}."
