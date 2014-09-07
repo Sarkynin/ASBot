19f5bf3ddf854a93ad0a38d02ccd974d789007f6
apt-get <badr.yacoubi@gmail.com>
2014-09-07 21:55:48 +0100

    fix

--- plugins/asbmove-plugin.rb
+++ plugins/asbmove-plugin.rb
@@ -13,7 +13,7 @@ class ASBMovePlugin
       if BotUtils.condense_name(row[0].gsub(" (Move)", "")) == BotUtils.condense_name(move)
         BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index][0].sub(" (Move)", "")} - #{$movesheet.rows[index][1]} | #{$movesheet.rows[index][2]} | #{$movesheet.rows[index][3]} | #{$movesheet.rows[index][6]} BAP | #{$movesheet.rows[index][8]} Acc | #{$movesheet.rows[index][9]} EN Cost | #{$movesheet.rows[index][10]} Eff% | Contact: #{$movesheet.rows[index][11]} | #{$movesheet.rows[index][12]} Prio | Combo Type: #{$movesheet.rows[index][13]} | Snatch: #{$movesheet.rows[index][14]} | Magic Coat: #{$movesheet.rows[index][15]}")
 
-        if BotUtils.condense_name(row[4]) == "table"
+        if BotUtils.condense_name(row[6]) == "table"
           BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index+1][1]}".gsub("é","e"))
           BotUtils.msgtype_reply(m, msgtype, "Please check the NDA for the table.")
         else
