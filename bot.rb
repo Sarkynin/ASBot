9b09e0f240702f4d72cd605e885d5a2ab4f2fa29
apt-get <badr.yacoubi@gmail.com>
2014-09-26 20:07:35 +0100

    disable recap

--- bot.rb
+++ bot.rb
@@ -11,16 +11,16 @@ bot = Cinch::Bot.new do
     c.nick = ENV["botircnick"]
     c.password = ENV["botircpass"]
     c.plugins.prefix = //
-    c.plugins.plugins = [SpeedPlugin, ASBMovePlugin, ASBStatsPlugin, ASBilityPlugin, ASBItemPlugin, ASBNaturePlugin, ASBTypePlugin, RollPlugin, BlamePlugin, NoPlugin, QuotesPlugin, ProfilePlugin, ComboPlugin, HelpPlugin, DCCheckPlugin, YesPlugin, ShufflePlugin, LearnPlugin, RecapPlugin]
+    c.plugins.plugins = [SpeedPlugin, ASBMovePlugin, ASBStatsPlugin, ASBilityPlugin, ASBItemPlugin, ASBNaturePlugin, ASBTypePlugin, RollPlugin, BlamePlugin, NoPlugin, QuotesPlugin, ProfilePlugin, ComboPlugin, HelpPlugin, DCCheckPlugin, YesPlugin, ShufflePlugin, LearnPlugin]
     c.plugins.options[QuotesPlugin] = {:quotes_address => ENV["quotes_url"]}
     c.plugins.options[ProfilePlugin] = {:smogon_username => ENV["smogon_username"], :smogon_password => ENV["smogon_password"]}
-    c.plugins.options[RecapPlugin] = {
-      :mode => :max_messages,
-      :max_messages => 25,
-      :time_format => "%H:%M",
-      :channels => c.channels,
-      :cooldown => 60
-    }
+    # c.plugins.options[RecapPlugin] = {
+    #   :mode => :max_messages,
+    #   :max_messages => 25,
+    #   :time_format => "%H:%M",
+    #   :channels => c.channels,
+    #   :cooldown => 60
+    # }
 
   end
 
