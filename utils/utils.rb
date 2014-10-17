04b658f3710da0e2d41126644eaa7b1d370484ae
apt-get <badr.yacoubi@gmail.com>
2014-10-17 13:07:48 +0100

    fix

--- utils/utils.rb
+++ utils/utils.rb
@@ -2,13 +2,6 @@ require 'gist'
 
 $googledrivesession = GoogleDrive.login(ENV["googledrivelogin"], ENV["googledrivepass"])
 
-$pokesheet = $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[4]
-$naturesheet = $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[5]
-$abilitysheet = $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[6]
-$typesheet = $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[7]
-$movesheet = $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[8]
-$itemsheet = $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[9].rows + $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[10].rows + $googledrivesession.spreadsheet_by_key("0AiVdR0Jv-e1hdDZxVlpFOW5yRGQxb3NDSmVqRHNxVUE").worksheets[11].rows
-
 Gist.login!(:username => ENV['githubnick'], :password => ENV["githubpass"])
 
 
