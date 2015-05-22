57da4c1eb649d4cbbfac5b61929f44374118c176
apt-get <badr.yacoubi@gmail.com>
2015-05-22 17:50:12 +0100

    fix once again

--- plugins/pick-plugin.rb
+++ plugins/pick-plugin.rb
@@ -8,13 +8,20 @@ class PickPlugin
 
   def execute m, msgtype, picks, optionstring
     options = optionstring.split(',').map{|option| option.strip}
-    argumenterror = "Please provide at least 2 arguments."
-    BotUtils.msgtype_reply(m, msgtype, argumenterror) if options.length < 2
+
+    if options.length < 2
+      argumenterror = "Please provide at least 2 arguments."
+      BotUtils.msgtype_reply(m, msgtype, argumenterror)
+      return
+    end
 
     picks.nil? ? picks = 1 : picks = picks.to_i
 
-    pickerror = "The number of picks should be inferior to the number of arguments."
-    BotUtils.msgtype_reply(m, msgtype, pickerror) if options.length < picks
+    if options.length < picks
+      pickerror = "The number of picks should be inferior to the number of arguments."
+      BotUtils.msgtype_reply(m, msgtype, pickerror)
+      return
+    end
 
     pickarray = Array.new
     picks.times { pickarray << options.sample }
