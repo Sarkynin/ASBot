35e7a100793cf70926a08f36a375a956dde4bd37
apt-get <badr.yacoubi@gmail.com>
2015-05-22 17:30:53 +0100

    Updated pick command to allow multiple pick choices.

--- plugins/pick-plugin.rb
+++ plugins/pick-plugin.rb
@@ -4,14 +4,25 @@ require_relative '../utils/utils.rb'
 class PickPlugin
   include Cinch::Plugin
 
-  match /^(!|@)pick (.+)$/i
+  match /^(!|@)pick(\d+)? (.+)$/i
 
-  def execute m, msgtype, optionstring
+  def execute m, msgtype, picks, optionstring
     options = optionstring.split(',').map{|option| option.strip}
-    if options.length < 2
-      BotUtils.msgtype_reply(m, msgtype, "Please provide at least 2 arguments.")
-    else
-      BotUtils.msgtype_reply(m, msgtype, options.sample)
+    argumenterror = "Please provide at least 2 arguments."
+    BotUtils.msgtype_reply(m, msgtype, argumenterror) if options.length < 2
+
+    pickarray = Array.new
+
+    picks.nil? ? picks = 1 : picks = picks.to_i
+
+    pickerror = "The number of picks should be inferior to the number of arguments."
+    BotUtils.msgtype_reply(m, msgtype, pickerror) if options.length < picks.length
+
+    picks.times do
+      pickarray << options.sample
+    end
+    pickstring = pickarray.join(', ')
+    BotUtils.msgtype_reply(m, msgtype, pickstring)
     end
   end
 end
