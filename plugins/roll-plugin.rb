bfbe7ccd55cd937af4149e1070223b9450b0128a
apt-get <badr.yacoubi@gmail.com>
2015-05-23 19:28:07 +0100

    add support for real random system for rollplugin (along with PRNG fallback)

--- plugins/roll-plugin.rb
+++ plugins/roll-plugin.rb
@@ -1,6 +1,6 @@
 require 'cinch'
 require_relative '../utils/utils.rb'
-
+require 'random/online'
 
 class RollPlugin
   include Cinch::Plugin
@@ -8,27 +8,36 @@ class RollPlugin
   match /^(!|@)roll(\d+)? ?(\d+)?/i
 
   def execute(m, msgtype, numberofdices, rollnumber)
-    rng = Random.new
-    if numberofdices.to_i >= 150
-      BotUtils.msgtype_reply(m, msgtype, "The number of rolls is too big. Please consider using less than 150 rolls multiple times to achieve your result.")
-    else
-      rollnumber.nil? ? rollnumber = 10000 : rollnumber = rollnumber.to_i
-      numberofdices.nil? ? numberofdices = 1 : numberofdices = numberofdices.to_i
-      if numberofdices == 1
-        rollednumber = rng.rand(1..rollnumber)
-        BotUtils.msgtype_reply(m, msgtype, "Rolled between 1 and #{rollnumber}: #{rollednumber}")
-      else
-        numberarray = Array.new
-        numbersstring = String.new
-        numberofdices.times do
-          numberarray << rng.rand(1..rollnumber)
-        end
-        numberarray.each do |rollednumber|
-          numbersstring << "#{rollednumber} "
-        end
-        BotUtils.msgtype_reply(m, msgtype, "Rolled #{numberofdices} dice between 1 and #{rollnumber}: #{numbersstring}")
-      end
-    end
-  end
 
+    if numberofdices.to_i >= 30
+      error = "The number of rolls is too big. Please consider using less than 30 rolls multiple times to achieve your result."
+      BotUtils.msgtype_reply(m, msgtype, error)
+      return
+    end
+
+    rollnumber.nil? ? rollnumber = 10000 : rollnumber = rollnumber.to_i
+    numberofdices.nil? ? numberofdices = 1 : numberofdices = numberofdices.to_i
+    numbers_string = String.new
+
+    begin
+      rng = RealRand::RandomOrg.new
+
+      numbers_string = rng.randnum(numberofdices, 1, rollnumber).join(", ")
+    rescue
+      rng = Random.new
+
+      numbers_array = Array.new
+      numbers_string = String.new
+
+      numberofdices.times { numbers_array << rng.rand(1..rollnumber) }
+      numbers_string = numbers_array.join(', ')        
+    end
+
+    if numberofdices == 1
+      BotUtils.msgtype_reply(m, msgtype, "Rolled between 1 and #{rollnumber}: #{numbers_string}")
+    else
+      BotUtils.msgtype_reply(m, msgtype, "Rolled #{numberofdices} dices between 1 and #{rollnumber}: #{numbers_string}")
+    end
+
+  end
 end
