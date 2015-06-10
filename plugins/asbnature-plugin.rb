61bc2fbb72ce355b72e3349415c348b42537ac1f
apt-get <badr.yacoubi@gmail.com>
2015-06-10 21:32:30 +0100

    add findnature

--- plugins/asbnature-plugin.rb
+++ plugins/asbnature-plugin.rb
@@ -5,9 +5,10 @@ require_relative '../utils/utils.rb'
 class ASBNaturePlugin
   include Cinch::Plugin
 
-  match /^(!|@)asbnature (\w+) ?(.*)?/i
+  match /^(!|@)asbnature (\w+) ?(.*)?/i,     method: :asbnature
+  match /^(!|@)findnature (.+)[ ]+(.+)/i,    method: :findnature
 
-  def execute(m, msgtype, nature, moodycheck)
+  def asbnature(m, msgtype, nature, moodycheck)
     $naturesheet.rows.each do |row|
       if BotUtils.condense_name(row[0]) == BotUtils.condense_name(nature)
         if moodycheck == 'moody'
@@ -17,6 +18,16 @@ class ASBNaturePlugin
         end
         return
       end
+    end
+    BotUtils.msgtype_reply(m, msgtype, "Nature not found.")
+  end
+
+  def findnature(m, msgtype, boost1, boost2)
+    $naturesheet.rows.each do |row|
+      if BotUtils.condense_name(row[1]).include?(BotUtils.condense_name(boost1)) && BotUtils.condense_name(row[1]).include?(BotUtils.condense_name(boost2))
+        BotUtils.msgtype_reply(m, msgtype, "The corresponding nature is #{row[0]}.")
+      end
+      return
     end
     BotUtils.msgtype_reply(m, msgtype, "Nature not found.")
   end
