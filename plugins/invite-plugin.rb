28c2bf757ece5a1ca24ae090fec860678625aa80
lc_guy <aptyget@gmail.com>
2016-01-30 23:05:25 +0000

    meme

--- plugins/invite-plugin.rb
+++ plugins/invite-plugin.rb
@@ -3,7 +3,7 @@ require 'cinch'
 class InvitePlugin
   include Cinch::Plugin
 
-  listen_to :invite, method: :on_connect
+  listen_to :invite, method: :on_invite
 
   def on_invite(m)
     bot.join(m.channel)
