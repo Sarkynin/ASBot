c7510e70655f315ae14e6cc2e33ba6fadc53daa1
apt-get <aptget@cock.lu>
2016-12-05 01:20:56 +0000

    fix after S/M

--- plugins/blame-plugin.rb
+++ plugins/blame-plugin.rb
@@ -6,6 +6,10 @@ class BlamePlugin
   match /^!blame (.+)/i
 
   def execute(m, user)
-    m.reply("Yeah, it's all #{user}'s fault. Shame on you.")
+    if user.downcase == 'asbot'
+      m.reply("Yeah, it's all my... hey, wait a minute!")
+    else
+      m.reply("Yeah, it's all #{user}'s fault. Shame on you.")
+    end
   end
 end
