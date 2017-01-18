232d642d23cc44770e29d0927500436303eb6248
apt-get <aptget@cock.lu>
2017-01-18 22:20:39 +0000

    yeah that was a bad idea

--- plugins/asbmove-plugin.rb
+++ plugins/asbmove-plugin.rb
@@ -13,7 +13,7 @@ class ASBMovePlugin
       #Move names on the sheet are followed by either " (Move)" or " (Command)",
       #so we need to remove those to verify the move
 
-      if BotUtils.condense_name(row[0].gsub(" (Move)", "").gsub(" (Command)","").gsub(" (Users)","")) == BotUtils.condense_name(move)
+      if BotUtils.condense_name(row[0].gsub(" (Move)", "").gsub(" (Command)","")) == BotUtils.condense_name(move)
         BotUtils.msgtype_reply(m, msgtype, "#{$movesheet.rows[index][0].sub(" (Move)", "")} - #{$movesheet.rows[index][1]} | #{$movesheet.rows[index][2]} | #{$movesheet.rows[index][3]} | #{$movesheet.rows[index][6]} BAP | #{$movesheet.rows[index][8]} Acc | #{$movesheet.rows[index][9]} EN Cost | #{$movesheet.rows[index][10]} Eff% | Contact: #{$movesheet.rows[index][11]} | #{$movesheet.rows[index][12]} Prio | Z-BAP: #{$movesheet.rows[index][13]} | Combo Type: #{$movesheet.rows[index][14]} | Snatch: #{$movesheet.rows[index][15]} | Magic Coat: #{$movesheet.rows[index][16]}")
         extralines = 2
 
